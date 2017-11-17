5cadfed11e32151b0f5e60270182615bd3d18fc8
spardit <jamemay19@gmail.com>
2017-11-17 11:19:02 +0700

    Successfuly information data

--- src/oop_lab/s359211110115/rmutsv/com/myfirstJAVA.java
+++ src/oop_lab/s359211110115/rmutsv/com/myfirstJAVA.java
@@ -3,6 +3,9 @@ package oop_lab.s359211110115.rmutsv.com;
 public class myfirstJAVA {
     public static void main(String[] args) {
         System.out.println ("Hello Wordi, I'm monjira");
+        System.out.println ("Information System");
+        System.out.println ("Faculty of Mangement Technolgy");
+        System.out.println ("rmutsv");
     }
 }
 
