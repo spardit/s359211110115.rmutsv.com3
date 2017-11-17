481bc7fce0fd193d907a2378450ad03e84b9a33a
sopardit <may140838@hotmail.com>
2017-11-17 10:38:42 +0700

    Initial commit

---
+++ src/oop_lab/s359211110115/rmutsv/com/myfirstJAVA.java
@@ -0,0 +1,12 @@
+package oop_lab.s359211110115.rmutsv.com;
+
+public class myfirstJAVA {
+    public static void main(String[] args) {
+        System.out.println ("Hello Wordi, I'm monjira");
+    }
+}
+
+
+
+
+
